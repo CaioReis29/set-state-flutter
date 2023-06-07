d3d0fa4f1a7d320360e7656bb3d4daa22378b0a6
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-06-06 22:41:44 -0300

    imc-calculator-flutter

---
+++ android/app/src/main/kotlin/br/com/setstate/set_state/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.setstate.set_state
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/imc_gauge.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:set_state/widgets/imc_gauge_range.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class ImcGauge extends StatelessWidget {
+
+  final double imc;
+
+  const ImcGauge({ Key? key, required this.imc }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return SfRadialGauge(
+                axes: [
+                  RadialAxis(
+                    showLabels: false,
+                    showAxisLine: false,
+                    showTicks: false,
+                    minimum: 12.5,
+                    maximum: 47.9,
+                    ranges: [
+                      ImcGaugeRange(
+                          color: Colors.blue[600]!,
+                          start: 12.5,
+                          end: 18.5,
+                          label: 'MAGREZA'),
+                      ImcGaugeRange(
+                          color: Colors.green[600]!,
+                          start: 18.5,
+                          end: 24.5,
+                          label: 'NORMAL'),
+                      ImcGaugeRange(
+                          color: Colors.yellow[600]!,
+                          start: 24.5,
+                          end: 29.9,
+                          label: 'SOBREPESO'),
+                      ImcGaugeRange(
+                          color: Colors.red[500]!,
+                          start: 29.9,
+                          end: 39.9,
+                          label: 'OBESIDADE'),
+                      ImcGaugeRange(
+                          color: Colors.red[900]!,
+                          start: 39.9,
+                          end: 47.9,
+                          label: 'OBESIDADE GRAVE'),
+                    ],
+                    pointers: [
+                      NeedlePointer(
+                        value: this.imc,
+                        enableDragging: true,
+                      )
+                    ],
+                  ),
+                ],
+              );
+  }
+}
